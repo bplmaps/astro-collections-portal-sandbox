9562347beb2fc6c8af52922f93ec7d0b52a3ee23
Garrett Dash Nelson <garrettdashnelson@gmail.com>
2023-07-12 10:53:51 -0400

    fix error in essays collection type that caused build error

--- src/content/config.ts
+++ src/content/config.ts
@@ -42,6 +42,6 @@ const itemCollection = defineCollection({
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-  'essay': essayCollection,
+  'essays': essayCollection,
   'item': itemCollection
 };
