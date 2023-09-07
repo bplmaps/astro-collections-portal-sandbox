753981c22950dc28bc8e0a585da83c4170b62d96
Garrett Dash Nelson <garrettdashnelson@gmail.com>
2023-09-07 18:06:22 -0400

    add title to clientLoadingItemCard

--- src/components/itemCard/clientLoadingItemCard.svelte
+++ src/components/itemCard/clientLoadingItemCard.svelte
@@ -12,7 +12,7 @@
     let title = data.response.document.title_info_primary_tsi;
     const exemplary_image = data.response.document.exemplary_image_ssi;
     const url = `https://bpldcassets.blob.core.windows.net/derivatives/images/${exemplary_image}/image_thumbnail_300.jpg`;
-    return url;
+    return {"title": title, "imgURL": url}
   }
 
   let data = getImageURL(record.id);
@@ -20,10 +20,13 @@
 
 <div>
     {#await data}
-    ...
+    <div class="italic text-gray-400 p-4">Loading record ...</div>
     {:then d}
-    <img src={d} alt="" class="w-full">
+    <img src={d.imgURL} alt="" class="w-full">
+    <div class="font-serif p-2">
+      {d.title}
+    </div>
     {:catch error}
-	<p style="color: red">Error loading this item</p>
+	  <p style="color: red">Error loading this item</p>
     {/await}
 </div>
