72de0b0299050d11e42fc899e825de15fa0450d1
Eric Robsky Huntley <ehuntley@mit.edu>
2023-10-13 07:02:58 -0400

    implement lazy loading using svelte-inview

--- src/components/itemCard/clientLoadingItemCard.svelte
+++ src/components/itemCard/clientLoadingItemCard.svelte
@@ -1,4 +1,17 @@
-<script>
+<script lang="ts">
+	import viewport from './viewportAction.js';
+  import { inview } from 'svelte-inview';
+  import type { ObserverEventDetails, Options } from 'svelte-inview';
+  
+  let isInView;
+  const options: Options = {
+    rootMargin: '50px',
+    unobserveOnEnter: true,
+  };
+
+  const handleChange = ({ detail }: CustomEvent<ObserverEventDetails>) =>
+    (isInView = detail.inView);
+
   export let record;
 
   async function getImageURL(id) {
@@ -9,18 +22,19 @@
       )}?format=json`
     );
     const data = await fetch(meta_endpoint).then((response) => response.json());
-    const exemplary_image = data.response.document.exemplary_image_ssi;
-    const url = `https://bpldcassets.blob.core.windows.net/derivatives/images/${exemplary_image}/image_thumbnail_300.jpg`;
+    const exemplaryImage = data.response.document.exemplary_image_ssi;
+    const url = `https://bpldcassets.blob.core.windows.net/derivatives/images/${exemplaryImage}/image_thumbnail_300.jpg`;
     return {"title": data.response.document.title_info_primary_tsi, "date": data.response.document.date_tsim, "imgURL": url}
   }
-
+  let test;
   let data = getImageURL(record.id);
 </script>
 
-<div>
+<div use:inview="{options}" on:inview_change="{handleChange}" class="min-h-[15vh]">
     {#await data}
     <div class="italic text-gray-400 p-4">Loading record ...</div>
     {:then d}
+    {#if isInView}
     <div>
       <img src={d.imgURL} alt="" class="w-full h-48 object-cover">
     </div>
@@ -30,6 +44,7 @@
     <div class="p-2 text-sm">
       {d.date}
     </div>
+    {/if}
     {:catch error}
 	  <p style="color: red">Error loading this item</p>
     {/await}
